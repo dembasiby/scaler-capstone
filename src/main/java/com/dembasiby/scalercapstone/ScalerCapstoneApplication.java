f5d327f20fa236374eae015a2889a24c5296dd9a
Demba Siby <dembasiby@gmail.com>
2025-04-26 20:12:40 +0000

    Initial commit

---
+++ src/main/java/com/dembasiby/scalercapstone/ScalerCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.dembasiby.scalercapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalerCapstoneApplication.class, args);
+    }
+
+}

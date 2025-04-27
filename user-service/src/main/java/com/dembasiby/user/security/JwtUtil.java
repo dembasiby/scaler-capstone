79810aed615000a9b993be7ad806c4a726d97dae
Demba Siby <dembasiby@gmail.com>
2025-04-27 20:21:30 +0000

    Created JwtUtil class, and generateToken & createToken methods

---
+++ user-service/src/main/java/com/dembasiby/user/security/JwtUtil.java
@@ -0,0 +1,41 @@
+package com.dembasiby.user.security;
+
+import com.dembasiby.user.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<>();c
+        claims.put("authorities", user.getAuthorities());
+        return createToken(claims, user.getUsername());
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        byte[] secretBytes = secret.getBytes(StandardCharsets.UTF_8);
+        SecretKey key = Keys.hmacShaKeyFor(secretBytes);
+        return Jwts.builder()
+                .claims(claims)
+                .subject(subject)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key, Jwts.SIG.HS256)
+                .compact();
+    }
+}

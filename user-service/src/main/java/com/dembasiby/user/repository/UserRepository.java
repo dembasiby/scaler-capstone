a0c4e5905aa64487ae9cd92bbe5085235389de86
Demba Siby <dembasiby@gmail.com>
2025-04-27 21:22:31 +0000

    Created User repository with 2 methods

---
+++ user-service/src/main/java/com/dembasiby/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.dembasiby.user.repository;
+
+import com.dembasiby.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+}
